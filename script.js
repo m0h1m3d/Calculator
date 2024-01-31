ed9d5096ce68ce756036ff80bae610c182c0c9bb
m0h1m3d <mohisham33@gmail.com>
2024-01-31 21:25:35 +0200

    update more user friendly ui

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const container = document.querySelector('.container');
+const opBtns = document.querySelectorAll('.op');
 const themeBtn = document.querySelector('.theme');
 const tBtns = document.querySelectorAll('.tBtn');
 
 themeBtn.addEventListener('click', (e)=>{
     if(e.target.tagName === 'BUTTON'){
         const target = e.target;
-        const shadowStyle = target.getAttribute('data-box-shadow')
+        const shadowStyle = target.getAttribute('data-box-shadow');
+        const bgColorStyle = target.getAttribute('data-bgcolor');
+        
         container.style.boxShadow = shadowStyle;
+        opBtns.forEach((op)=>{
+          op.style.backgroundColor = bgColorStyle;
+        })
     }
 })
 
