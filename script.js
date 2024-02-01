cbddaf3f038d544d0e507b8d59a81d4c4e0e2d1d
m0h1m3d <mohisham33@gmail.com>
2024-02-01 22:16:22 +0200

    add basic operations logic

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ const digits = document.querySelectorAll(".digit");
 let allowDecim = true;
 
 let state = {
-  opA: '',
-  opB: '',
-  operator: ''
+  opA: "",
+  opB: "",
+  operator: "",
 };
 
 themeEl.addEventListener("click", (e) => {
@@ -33,71 +33,112 @@ themeEl.addEventListener("click", (e) => {
   }
 });
 
-const displayNum = function(e){
-    if (
-      e.target.classList.contains("digit") ||
-      e.target.classList.contains("op")
-    ) {
-      const target = e.target;
-
-      if(solDisplay.textContent.split("").length >= 9) solDisplay.style.fontSize = "1.5rem";
-      if(solDisplay.textContent.split("").length >= 39) return;  
-      
-      solDisplay.textContent += target.textContent;
-      
-    }
-};
-
-const clearDis = function(e){
+const displayNum = function (e) {
   if (
-    e.target.classList.contains("clear")
-  ) {
-    solDisplay.style.fontSize = '3rem';
-    solDisplay.textContent = '';
-    prevDisplay.textContent = '';
-    state.opA = '';
-    state.opB = '';
-    state.operator = '';
-    allowDecim = !allowDecim;
-  };
-};
-
-const deleteNum = function(e){
-  if (
-    e.target.classList.contains("del")
-  ) {
-    const deletedNum = [...solDisplay.textContent].pop();
-    if(deletedNum === '.') allowDecim = !allowDecim;
-    
-    solDisplay.textContent = solDisplay.textContent.replace(deletedNum, '');
-    if(solDisplay.textContent.split("").length < 9) solDisplay.style.fontSize = "3rem";
-  };
-}
-
-const addNegative = function(e){
-  if (
-    e.target.classList.contains("neg")
-  ) {
-    solDisplay.textContent ? solDisplay.textContent = -solDisplay.textContent : '';
-  };
-}
-
-const addDecimal = function(e){
-  if (
-    e.target.classList.contains("decim")
+    e.target.classList.contains("digit") ||
+    e.target.classList.contains("op")
   ) {
     const target = e.target;
-    if(allowDecim){
-      solDisplay.textContent += '.';
+
+    if (solDisplay.textContent.split("").length >= 9)
+      solDisplay.style.fontSize = "1.5rem";
+    if (solDisplay.textContent.split("").length >= 39) return;
+
+    solDisplay.textContent += target.textContent;
+  }
+};
+
+const clearDis = function (e) {
+  if (e.target.classList.contains("clear")) {
+    solDisplay.style.fontSize = "3rem";
+    solDisplay.textContent = "";
+    prevDisplay.textContent = "";
+    state.opA = "";
+    state.opB = "";
+    state.operator = "";
+    allowDecim = !allowDecim;
+  }
+};
+
+const deleteNum = function (e) {
+  if (e.target.classList.contains("del")) {
+    const deletedNum = [...solDisplay.textContent].pop();
+    if (deletedNum === ".") allowDecim = !allowDecim;
+
+    solDisplay.textContent = solDisplay.textContent.replace(deletedNum, "");
+    if (solDisplay.textContent.split("").length < 9)
+      solDisplay.style.fontSize = "3rem";
+  }
+};
+
+const addNegative = function (e) {
+  if (e.target.classList.contains("neg")) {
+    solDisplay.textContent
+      ? (solDisplay.textContent = -solDisplay.textContent)
+      : "";
+  }
+};
+
+const addDecimal = function (e) {
+  if (e.target.classList.contains("decim")) {
+    const target = e.target;
+    if (allowDecim) {
+      solDisplay.textContent += ".";
       allowDecim = !allowDecim;
     }
-  };
+  }
+};
+
+const equals = function(e,a,b,o){
+  if (e.target.classList.contains("eq")) {
+    prevDisplay.textContent = solDisplay.textContent.replace('=','');
+    prevDisplay.style.opacity = '0.5';
+    prevDisplay.style.fontSize = '2rem';
+    solDisplay.textContent = '';
+  }
 }
 
-btnsEl.addEventListener("click", (e)=>{
+const add = function (a, b) {
+  console.log("addition", a + b);
+  return a + b;
+};
+
+const subs = function (a, b) {
+  console.log("substraction", a - b);
+  return a - b;
+};
+
+const multi = function (a, b) {
+  console.log("multiplication", a * b);
+  return a * b;
+};
+
+const devide = function (a, b) {
+  console.log("divition", a / b);
+  return a / b;
+};
+
+const operate = function (a, b, o) {
+  state.opA = a;
+  state.opB = b;
+  state.operator = o;
+
+  o === "+"
+    ? add(a, b)
+    : o === "-"
+    ? subs(a, b)
+    : o === "x"
+    ? multi(a, b)
+    : o === "÷"
+    ? devide(a, b)
+    : "";
+};
+
+btnsEl.addEventListener("click", (e) => {
   clearDis(e);
   deleteNum(e);
   displayNum(e);
   addNegative(e);
-  addDecimal(e)
+  addDecimal(e);
+  equals(e);
 });
